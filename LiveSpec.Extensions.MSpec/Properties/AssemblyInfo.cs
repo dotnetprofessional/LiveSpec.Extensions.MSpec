5434b0409a6ba1f7f1a6091a8e18a4b64dc494a4
dnp <dnp@dotnetprofessional.com>
2015-04-06 19:34:15 -0700

    Updated nuspec and assembly details.

--- LiveSpec.Extensions.MSpec/Properties/AssemblyInfo.cs
+++ LiveSpec.Extensions.MSpec/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("LiveSpec.Extensions.MSpec")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extensions to the Machine.Specification library to support additional BDD/Specification by Example features. Also designed to support the LiveSpec reporting system.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dotNetProfessional")]
 [assembly: AssemblyProduct("LiveSpec.Extensions.MSpec")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
